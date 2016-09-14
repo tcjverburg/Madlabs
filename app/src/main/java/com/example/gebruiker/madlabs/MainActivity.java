9959df000797195f14862cfc48c035b06e6cd041
tcjverburg <tom_verburg@hotmail.nl>
2016-09-14 14:45:20 +0200

    Followed tutorial and should be able to go from the first activity to the second activity by the click of a button. Must look into auto generated api indexing by android studio.

--- app/src/main/java/com/example/gebruiker/madlabs/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/madlabs/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gebruiker.madlabs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //bla
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onWordInputClick(View view) {
+        Intent getNameScreen = new Intent(this, WordInput.class);
+        final int result=1;
+        getNameScreen.putExtra("callingActivity", "MainActivity");
+        startActivity(getNameScreen);
     }
 }
---
+++ app/src/main/java/com/example/gebruiker/madlabs/WordInput.java
@@ -0,0 +1,14 @@
+package com.example.gebruiker.madlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Gebruiker on 14-9-2016.
+ */
+public class WordInput extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
