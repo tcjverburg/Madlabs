b9eb09bd2919855bd3f6302384fa5ceb2297c90d
tcjverburg <tom_verburg@hotmail.nl>
2016-09-14 15:41:50 +0200

    Link between first and second activity done. Android back button seems to work without me having added anything. Should look into.

--- app/src/main/java/com/example/gebruiker/madlabs/WordInput.java
+++ app/src/main/java/com/example/gebruiker/madlabs/WordInput.java
@@ -1,6 +1,7 @@
 package com.example.gebruiker.madlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -10,5 +11,8 @@ public class WordInput extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_input);
+        Intent activityThatCalled = getIntent();
+        String previousActivity = (String) activityThatCalled.getExtras().get("callingActivity");
     }
 }
